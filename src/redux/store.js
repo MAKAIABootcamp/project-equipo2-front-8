f6ee8d170a3fd4e01b4c89915b7246e7b3928968
Tatiana García <t.dvla8549@gmail.com>
2024-10-02 19:03:56 -0500

    Actualizando Store y Home

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import  tipsReducer from "./tips/tipsSlice"
+import carruselReducer from "./Home/carruselSlice"
+import beneficiosReducer from "./Home/beneficiosSlice"
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         tips: tipsReducer,
+        carrusel: carruselReducer,
+        beneficios: beneficiosReducer,
     }
 })
 
