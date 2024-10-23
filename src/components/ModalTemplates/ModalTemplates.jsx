1a83f5f668a85ab8bf71ff96a19422499edd1452
Daniela Araque Sanchez <Dani.a.sanchez1106@hotmail.com>
2024-10-23 11:43:28 -0500

    Agregando modal para previsualizar imagen de plantillas

---
+++ src/components/ModalTemplates/ModalTemplates.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+const ModalTemplates = ({image, onClose }) => {
+    return (
+      <div
+        className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-10"
+        onClick={onClose}
+      >
+        <div
+          className="bg-white p-4 rounded-lg shadow-lg max-w-[90%] max-h-[90%] overflow-auto relative"
+          onClick={(e) => e.stopPropagation()} // Evita cerrar al hacer clic dentro
+        >
+          <img
+            src={image}
+            alt="Imagen seleccionada"
+            className="w-auto h-auto max-h-[80vh] object-contain"
+          />
+          <button
+            className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
+            onClick={onClose}
+          >
+            ✖
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+  ModalTemplates.propTypes = {
+    image: PropTypes.string.isRequired,
+    onClose: PropTypes.string.isRequired,
+  };
+
+  export default ModalTemplates;
--- src/pages/TemplatesCV/TemplatesCV.jsx
+++ src/pages/TemplatesCV/TemplatesCV.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import {
+  EffectCoverflow,
+  Pagination,
+  Autoplay,
+} from "swiper/modules";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchTemplates } from "../../redux/templates/templatesSlice";
+import Modal from '../../components/ModalTemplates/ModalTemplates';
 
 //Imagenes
 import Step1Image from "../../assets/Capa 2.svg";
 import Step2Image from "../../assets/Descargar 1.svg";
 import Step3Image from "../../assets/Group 8.svg";
 
-
 const Templates = () => {
   const dispatch = useDispatch();
   const { templates, status, error } = useSelector((state) => state.templates);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const openModal = (image) => setSelectedImage(image);
+  const closeModal = () => setSelectedImage(null);
 
   useEffect(() => {
     dispatch(fetchTemplates());
@@ -28,7 +38,7 @@ const Templates = () => {
     <div className="min-h-screen bg-gray-50 py-10 px-5">
       <div className="bg-white py-12 px-4 lg:px-8">
         <h2 className="text-center text-3xl font-bold text-gray-800 mb-8 font-montserrat">
-          Explora las plantillas que tenemos disponibles para ti
+          Explora las plantillas que tenemos disponibles para ti.
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-16 w-full px-8 py-6 relative">
@@ -41,15 +51,15 @@ const Templates = () => {
               />
               <img
                 src={Step1Image}
-                alt="Muñeca"
-                className="relative w-[150px] h-[150px] z-10 transform transition-transform duration-300 hover:scale-110 hover:translate-y-1"
+                alt="Paso 3"
+                className="relative w-40 h-40 z-10 mb-3 transform transition-transform duration-300 hover:scale-110 hover:translate-y-1"
               />
             </div>
             <h3 className="mt-6 text-xl font-semibold font-montserrat text-gray-900 z-20">
               Paso 1
             </h3>
             <p className="text-gray-600 text-center z-20 font-dosis">
-              Revisa las diferentes opciones que se acomoden a tus gustos.
+            Descubre las plantillas que se ajusten a tu estilo y necesidades.
             </p>
           </div>
 
@@ -85,13 +95,15 @@ const Templates = () => {
               />
               <img
                 src={Step2Image}
-                alt="Paso 2"
+                alt="Paso 3"
                 className="relative w-40 h-40 z-10 mb-3 transform transition-transform duration-300 hover:scale-110 hover:translate-y-1"
               />
             </div>
-            <h3 className="text-xl font-semibold font-montserrat text-gray-900">Paso 2</h3>
+            <h3 className="text-xl font-semibold font-montserrat text-gray-900">
+              Paso 2
+            </h3>
             <p className="text-gray-600 text-center font-dosis">
-              Una vez seleccionada la plantilla, descárgala en formato Word.
+            ¡Encontraste la ideal! Descárgala fácilmente en formato Word.
             </p>
           </div>
 
@@ -129,83 +141,80 @@ const Templates = () => {
               <img
                 src={Step3Image}
                 alt="Paso 3"
-                className="relative w-40 h-40 z-10 mb-3 transform transition-transform duration-300 hover:scale-110 hover:translate-y-1"
+                className="w-full h-auto max-h-64 object-contain mb-4 rounded-md"
               />
             </div>
-            <h3 className="text-xl font-semibold font-montserrat text-gray-900">Paso 3</h3>
+            <h3 className="text-xl font-semibold font-montserrat text-gray-900">
+              Paso 3
+            </h3>
             <p className="text-gray-600 text-center font-dosis">
-              Edita la hoja de vida que acabas de descargar con tu información.
+            Llénala con tus datos y prepárate para destacar con tu nueva hoja de vida.
             </p>
           </div>
         </div>
       </div>
 
       <div className="bg-white py-12 px-4 lg:px-8">
-        <Swiper
-          spaceBetween={20}
-          slidesPerView={4}
-          slidesPerGroup={3}
-          autoplay={{ delay: 3000 }}
-          pagination={{
-            clickable: true,
-            el: ".swiper-pagination",
-            renderBullet: (index, className) => {
-              return `<span class="${className} bg-color-5 "></span>`;
-            },
-          }}
-          navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-          }}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="relative w-full h-auto pb-16"
-          breakpoints={{
-            0: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            320: {
-              slidesPerView: 1,
-              slidesPerGroup: 1,
-            },
-            640: {
-              slidesPerView: 2,
-              slidesPerGroup: 2,
-            },
-            768: {
-              slidesPerView: 3,
-              slidesPerGroup: 3,
-            },
-            1024: {
-              slidesPerView: 4,
-              slidesPerGroup: 4,
-            },
-          }}
-        >
-          {templates.map((template, index) => (
-            <SwiperSlide key={index} className="max-w-xs">
-              <div className="flex flex-col items-center p-4 bg-gray-50 rounded-lg shadow-lg">
-                <img
-                  src={template.imageURL}
-                  alt={template.Nombre}
-                  className="w-full h-auto max-h-64 object-contain mb-4 rounded-md"
-                />
-                <a
-                  href={template.downloadURL}
-                  className="px-6 py-2 rounded-lg mt-4 mb-4 border border-color-1 text-color-3 hover:bg-[#ece1ff] hover:text-color-3 transition font-dosis"
-                >
-                  Descargar
-                </a>
-              </div>
-            </SwiperSlide>
-          ))}
+      <Swiper
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 15,
+          depth: 200,
+          modifier: 1.5,
+          slideShadows: false,
+        }}
+        autoplay={{ delay: 3000 }}
+        pagination={{
+          clickable: true,
+          el: ".swiper-pagination",
+          renderBullet: (index, className) =>
+            `<span class="${className} bg-color-5"></span>`,
+        }}
+        modules={[EffectCoverflow, Pagination, Autoplay,]}
+        className="relative w-full h-auto pb-16"
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          640: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        {templates.map((template, index) => (
+          <SwiperSlide key={index} className="max-w-xs">
+            <button
+              onClick={() => openModal(template.imageURL)}
+              className="flex flex-col items-center p-4 bg-gray-50 rounded-lg shadow-lg"
+            >
+              <img
+                src={template.imageURL}
+                alt={template.Nombre}
+                className="w-full max-w-[400px] sm:max-w-[300px] lg:max-w-[400px] h-auto object-contain mb-4 rounded-md"
+              />
+              <a
+                href={template.downloadURL}
+                download
+                className="px-6 py-2 rounded-lg mt-4 mb-4 border border-color-1 text-color-3 hover:bg-[#ece1ff] hover:text-color-3 transition font-dosis"
+              >
+                Descargar
+              </a>
+            </button>
+          </SwiperSlide>
+        ))}
 
-          <div className="swiper-button-next absolute right-0 top-1/2 transform -translate-y-1/2 text-color-5 z-10" />
-          <div className="swiper-button-prev absolute left-0 top-1/2 transform -translate-y-1/2 text-color-5 z-10" />
+        {/* Paginación y Botones de Navegación */}
+        <div className="swiper-pagination !absolute !bottom-[-5px] !left-1/2 !transform !-translate-x-1/2 z-20"></div>
+      </Swiper>
 
-          <div className="swiper-pagination !absolute !bottom-[-0px] !left-1/2 !transform !-translate-x-1/2 z-20"></div>
-        </Swiper>
-      </div>
+      {selectedImage && (
+        <Modal  
+          image={selectedImage} 
+          onClose={closeModal} 
+        />
+      )}
+    </div>
     </div>
   );
 };
