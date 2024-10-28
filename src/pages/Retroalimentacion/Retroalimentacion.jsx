d792d06021560b98888ab017b5e43c01f464d732
Angie Liceth <liceth5262@gmail.com>
2024-10-27 21:17:10 -0500

    modificacion del diseño en la pagina de retroalimentacion

--- src/pages/Retroalimentacion/Retroalimentacion.jsx
+++ src/pages/Retroalimentacion/Retroalimentacion.jsx
@@ -1,88 +1,174 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination} from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { useParams } from "react-router-dom";
-import psicolaboral from "../../assets/psicolaboral.jpg";
-import { fetchFeedback, fetchChat } from "../../redux/InterviewSimulator/InterviewSimulatorSlice"; 
+import tecnica from "../../assets/tecnica.jpg";
+import { fetchFeedback, fetchChat } from "../../redux/InterviewSimulator/InterviewSimulatorSlice";
+import { FaStar, FaRegStar } from "react-icons/fa";
+import imagen from "../../assets/retroa-3.png";
 
 const Retroalimentacion = () => {
     const { intentoId } = useParams();
     const dispatch = useDispatch();
 
-    // Obtener datos del Redux state
     const feedbacks = useSelector((state) => state.interview.feedbacks);
-    const chatHistory = useSelector((state) => state.interview.messages?.chatHistory);
+    const chatHistory = useSelector(
+        (state) => state.interview.messages?.chatHistory
+    );
 
-    // Manejar el intentoId y cargar la data necesaria
+    const [showCards, setShowCards] = useState([false, false, false]);
+    const [showFullText, setShowFullText] = useState([]);
+    const [hoveredParam, setHoveredParam] = useState(null); // Estado para el tooltip
+
     useEffect(() => {
-        // if (intentoId && !feedbacks?.feedback) {
-        //     dispatch(fetchFeedback(intentoId));
-        // }
-        // if (intentoId && !chatHistory) {
-        //     dispatch(fetchChat(intentoId));
-        // }
         if (intentoId) {
             dispatch(fetchFeedback(intentoId));
             dispatch(fetchChat(intentoId));
         }
     }, [intentoId, dispatch]);
 
+    const activateSlideEffect = () => {
+        setShowCards([false, false, false]);
+        const timeouts = [
+            setTimeout(() => setShowCards([true, false, false]), 300),
+            setTimeout(() => setShowCards([true, true, false]), 600),
+            setTimeout(() => setShowCards([true, true, true]), 900),
+        ];
+
+        return () => timeouts.forEach(clearTimeout);
+    };
+
+    useEffect(() => {
+        activateSlideEffect();
+    }, []);
+
+    const renderStars = (score) => {
+        const totalStars = 5;
+        const filledStars = Math.ceil(score / 2);
+
+        return (
+            <>
+                {[...Array(totalStars)].map((_, index) =>
+                    index < filledStars ? (
+                        <FaStar key={index} className="text-yellow-500" />
+                    ) : (
+                        <FaRegStar key={index} className="text-gray-700" />
+                    )
+                )}
+            </>
+        );
+    };
+
+    const toggleFullText = (index) => {
+        setShowFullText((prev) => {
+            const newShowFullText = [...prev];
+            newShowFullText[index] = !newShowFullText[index];
+            return newShowFullText;
+        });
+    };
+
+    const paramDescription = {
+        claridad: "Evalúa qué tan comprensible es tu respuesta.",
+        formalidad: "Mide el tono y el nivel de profesionalismo en tu respuesta.",
+        relevancia: "Evalúa si tu respuesta está alineada con la pregunta planteada."
+    };
+
     return (
-        <div className="relative">
-            <section className="flex justify-center">
-                <h1 className="font-montserrat font-bold text-4xl py-10">
-                    Feedback de la entrevista que acabas de realizar
+        <div className="relative mb-10">
+            <section className="flex md:flex-row flex-col items-center justify-evenly">
+                <img className="md:w-1/6 w-1/3" src={imagen} alt="" />
+                <h1 className="font-montserrat font-bold text-center md:text-4xl text-2xl py-10 w-4/5">
+                    Evaluamos la claridad, relevancia y formalidad de tus preguntas
                 </h1>
             </section>
-
-            {/* Imagen de fondo */}
+            <section className="py-4">
+                <p className="font-dosis md:text-3xl text-xl text-center">Desliza para ver el feedback de cada pregunta</p>
+            </section>
             <section className="relative">
-                <img src={psicolaboral} alt="Fondo" className="w-full h-screen object-cover" />
+                <img
+                    src={tecnica}
+                    alt="Fondo"
+                    className="w-full h-screen object-cover"
+                />
 
-                {/* Slider de Swiper con navegación y paginación */}
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={1}
-                    navigation 
-                    pagination={{ clickable: true }} 
-                    modules={[Navigation, Pagination]} 
-                    className="absolute inset-0" 
+                    pagination={{
+                        clickable: true,
+                        renderBullet: (index, className) => {
+                            return `<span class="${className} bg-white"></span>`;
+                        },
+                    }}
+                    navigation={{
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    }}
+                    modules={[Navigation, Pagination]}
+                    className="absolute inset-0"
+                    onSlideChange={activateSlideEffect}
                 >
-                    {/* Mapear las preguntas y feedbacks */}
                     {chatHistory?.map((chat, index) => (
                         <SwiperSlide key={index}>
-                            <div className="flex flex-col justify-center items-center h-full text-white px-10 bg-gray-900 bg-opacity-75">
-                                <h2 className="text-3xl font-bold mb-6">{chat.question}</h2>
+                            <div className="flex flex-col justify-center items-center h-full text-white bg-gray-900 bg-opacity-75 font-dosis">
+                                <div className="p-4 bg-color-2 bg-opacity-35 rounded-lg mb-6">
+                                    <h2 className="text-3xl text-center font-bold ">{chat.question}</h2>
+                                </div>
 
-                                {/* Mostrar el feedback relacionado con la pregunta */}
                                 {feedbacks?.feedback?.[index] && (
                                     <>
-                                        <p className="text-center max-w-3xl mb-10">
-                                            {feedbacks.feedback[index].sugerencias}
-                                        </p>
-                                        <div className="flex justify-center space-x-8">
-                                            <div className="p-4 bg-gray-900 bg-opacity-75 rounded-lg text-center">
-                                                <h3 className="font-semibold text-lg">Claridad</h3>
-                                                <p>{feedbacks.feedback[index].claridad}</p>
-                                            </div>
-                                            <div className="p-4 bg-gray-900 bg-opacity-75 rounded-lg text-center">
-                                                <h3 className="font-semibold text-lg">Formalidad</h3>
-                                                <p>{feedbacks.feedback[index].formalidad}</p>
-                                            </div>
-                                            <div className="p-4 bg-gray-900 bg-opacity-75 rounded-lg text-center">
-                                                <h3 className="font-semibold text-lg">Relevancia</h3>
-                                                <p>{feedbacks.feedback[index].relevancia}</p>
-                                            </div>
+                                        <div className="item flex md:flex-row flex-col justify-center space-x-8">
+                                            {["claridad", "formalidad", "relevancia"].map(
+                                                (param, idx) => (
+                                                    <div
+                                                        key={param}
+                                                        className={`relative p-4 bg-color-2 bg-opacity-35 rounded-lg text-center transition-all duration-500 transform ${showCards[idx]
+                                                            ? "translate-x-0 opacity-100"
+                                                            : "translate-x-10 opacity-0"
+                                                            }`}
+                                                        onMouseEnter={() => setHoveredParam(param)}
+                                                        onMouseLeave={() => setHoveredParam(null)}
+                                                    >
+                                                        <h3 className="font-semibold text-lg capitalize">
+                                                            {param}
+                                                        </h3>
+                                                        <div className="flex justify-center">
+                                                            {renderStars(feedbacks.feedback[index][param])}
+                                                        </div>
+
+                                                        {hoveredParam === param && (
+                                                            <div className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-48 p-2 text-sm text-white bg-gray-700 rounded opacity-90 transition-opacity duration-300">
+                                                                {paramDescription[param]}
+                                                            </div>
+                                                        )}
+                                                    </div>
+                                                )
+                                            )}
+                                        </div>
+                                        <div className="mt-6">
+                                            <p className="text-center max-w-3xl p-4 bg-color-2 bg-opacity-35 rounded-lg ">
+                                                {showFullText[index]
+                                                    ? feedbacks.feedback[index].sugerencias
+                                                    : `${feedbacks.feedback[index].sugerencias.slice(0, 100)}...`}
+                                                <button
+                                                    className="text-color-5 underline "
+                                                    onClick={() => toggleFullText(index)}
+                                                >
+                                                    {showFullText[index] ? "Ver menos" : "Ver más"}
+                                                </button>
+                                            </p>
                                         </div>
                                     </>
                                 )}
                             </div>
                         </SwiperSlide>
                     ))}
+                    <div className="swiper-button-next text-color-2 hover:text-color-1" />
+                    <div className="swiper-button-prev text-color-2 hover:text-color-1" />
                 </Swiper>
             </section>
         </div>
